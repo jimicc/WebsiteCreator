674d8f9e67ac8026ccd232fef57fada72fed55fd
jimicc1 <jimicc94@gmail.com>
2019-09-10 23:41:21 +0200

    Cleaning

--- passenger_wsgi.py
+++
@@ -1,5 +0,0 @@
-import sys, os
-sys.path.append(os.getcwd())
-os.environ['DJANGO_SETTINGS_MODULE'] = "cms.settings" 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
